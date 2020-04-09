af376235f6ba10d780f4a1e6f0b790a665c9e294
jossanjim3 <jossanjim3@gmail.com>
2020-04-09 17:46:05 +0200

    splash activity

---
+++ app/src/main/java/us/master/entregable1/SplashActivity.java
@@ -0,0 +1,25 @@
+package us.master.entregable1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private int SPLASH_DURATION = 1000; // miliseconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        },SPLASH_DURATION);
+    }
+}
